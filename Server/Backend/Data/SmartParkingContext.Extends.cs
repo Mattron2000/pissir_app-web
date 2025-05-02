f75856c042c5c11d5c4b88ee26b574f3eca9e20d
Palmieri Matteo <88315099+Mattron2000@users.noreply.github.com>
2025-05-02 10:41:42 +0200

    Extend SmartParkingContext with seeding and check constraints

---
+++ Server/Backend/Data/SmartParkingContext.Extends.cs
@@ -0,0 +1,121 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data;
+
+public partial class SmartParkingContext
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Required for extensibility in partial methods and compatibility with EF Core's instance-based model configuration."
+    )]
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UsersType>(entity =>
+        {
+            // Seed enum data
+            entity.HasData(
+                Enum.GetValues(typeof(UsersTypeEnum))
+                    .Cast<UsersTypeEnum>()
+                    .Select(e => new UsersType { Name = e.ToString() })
+            );
+        });
+
+        modelBuilder.Entity<PricesType>(entity =>
+        {
+            // Seed enum data
+            entity.HasData(
+                Enum.GetValues(typeof(PricesTypeEnum))
+                    .Cast<PricesTypeEnum>()
+                    .Select(e => new PricesType { Name = e.ToString() })
+            );
+        });
+
+        modelBuilder.Entity<Price>(entity =>
+        {
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_Amount_Positive", "\"amount\" > 0");
+            });
+
+            // Seed prices data
+            entity.HasData(
+                new Price { Type = PricesTypeEnum.PARKING.ToString(), Amount = 5.5M },
+                new Price { Type = PricesTypeEnum.CHARGING.ToString(), Amount = 9.5M }
+            );
+        });
+
+        modelBuilder.Entity<SlotsStatus>(entity =>
+        {
+            //Seed enum data
+            entity.HasData(
+                Enum.GetValues(typeof(SlotsStatusEnum))
+                    .Cast<SlotsStatusEnum>()
+                    .Select(e => new SlotsStatus { Status = e.ToString() })
+            );
+        });
+
+        modelBuilder.Entity<Slot>(entity =>
+        {
+            // Set autoincrement
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+            // Seed slot data
+            entity.HasData(
+                Enumerable.Range(1, 5).Select(id => new Slot
+                {
+                    Id = id,
+                    Status = SlotsStatusEnum.FREE.ToString()
+                })
+            );
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_User_Email_Format", "\"email\" LIKE '%_@%_.%'");
+                t.HasCheckConstraint("CK_User_Password_Length", "LENGTH(\"password\") >= 8");
+                t.HasCheckConstraint("CK_User_Name_Length", "LENGTH(\"name\") > 0");
+                t.HasCheckConstraint("CK_User_Surname_Length", "LENGTH(\"surname\") > 0");
+            });
+
+            // Seed admin user data
+            entity.HasData(
+                new User {
+                    Email = "admin@gmail.com",
+                    Password = "adminadmin",
+                    Type = UsersTypeEnum.ADMIN.ToString(),
+                    Name = "Matteo",
+                    Surname = "Palmieri",
+                }
+            );
+        });
+
+        modelBuilder.Entity<Request>(entity =>
+        {
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_Request_Kw_Valid", "\"kw\" IS NULL OR (\"kw\" > 0 AND \"kw\" <= 100)");
+                t.HasCheckConstraint("CK_Request_Datetime_Valid", "\"datetime_start\" < \"datetime_end\"");
+            });
+        });
+
+        modelBuilder.Entity<Reservation>(entity =>
+        {
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_Reservation_Datetime_Valid", "\"datetime_start\" < \"datetime_end\"");
+            });
+        });
+
+        modelBuilder.Entity<Fine>(entity =>
+        {
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_Fine_Datetime_Valid", "\"datetime_start\" < \"datetime_end\"");
+            });
+        });
+    }
+}
---
+++ Server/Backend/Migrations/20250502083722_AddSeedAndCheckInDB.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedAndCheckInDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "prices_type",
+                column: "name",
+                values: new object[]
+                {
+                    "CHARGING",
+                    "PARKING"
+                });
+
+            migrationBuilder.InsertData(
+                table: "slots_status",
+                column: "status",
+                values: new object[]
+                {
+                    "FREE",
+                    "OCCUPIED"
+                });
+
+            migrationBuilder.InsertData(
+                table: "users_type",
+                column: "name",
+                values: new object[]
+                {
+                    "ADMIN",
+                    "BASE",
+                    "PREMIUM"
+                });
+
+            migrationBuilder.InsertData(
+                table: "prices",
+                columns: new[] { "type", "amount" },
+                values: new object[,]
+                {
+                    { "CHARGING", 9.5m },
+                    { "PARKING", 5.5m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "slots",
+                columns: new[] { "id", "status" },
+                values: new object[,]
+                {
+                    { 1, "FREE" },
+                    { 2, "FREE" },
+                    { 3, "FREE" },
+                    { 4, "FREE" },
+                    { 5, "FREE" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "email", "name", "password", "surname", "type" },
+                values: new object[] { "admin@gmail.com", "Matteo", "adminadmin", "Palmieri", "ADMIN" });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_User_Email_Format",
+                table: "users",
+                sql: "\"email\" LIKE '%_@%_.%'");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_User_Name_Length",
+                table: "users",
+                sql: "LENGTH(\"name\") > 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_User_Password_Length",
+                table: "users",
+                sql: "LENGTH(\"password\") >= 8");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_User_Surname_Length",
+                table: "users",
+                sql: "LENGTH(\"surname\") > 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Reservation_Datetime_Valid",
+                table: "reservations",
+                sql: "\"datetime_start\" < \"datetime_end\"");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Request_Datetime_Valid",
+                table: "requests",
+                sql: "\"datetime_start\" < \"datetime_end\"");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Request_Kw_Valid",
+                table: "requests",
+                sql: "\"kw\" IS NULL OR (\"kw\" > 0 AND \"kw\" <= 100)");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Amount_Positive",
+                table: "prices",
+                sql: "\"amount\" > 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Fine_Datetime_Valid",
+                table: "fines",
+                sql: "\"datetime_start\" < \"datetime_end\"");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_User_Email_Format",
+                table: "users");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_User_Name_Length",
+                table: "users");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_User_Password_Length",
+                table: "users");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_User_Surname_Length",
+                table: "users");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Reservation_Datetime_Valid",
+                table: "reservations");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Request_Datetime_Valid",
+                table: "requests");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Request_Kw_Valid",
+                table: "requests");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Amount_Positive",
+                table: "prices");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Fine_Datetime_Valid",
+                table: "fines");
+
+            migrationBuilder.DeleteData(
+                table: "prices",
+                keyColumn: "type",
+                keyValue: "CHARGING");
+
+            migrationBuilder.DeleteData(
+                table: "prices",
+                keyColumn: "type",
+                keyValue: "PARKING");
+
+            migrationBuilder.DeleteData(
+                table: "slots",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "slots",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "slots",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "slots",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "slots",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "slots_status",
+                keyColumn: "status",
+                keyValue: "OCCUPIED");
+
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "email",
+                keyValue: "admin@gmail.com");
+
+            migrationBuilder.DeleteData(
+                table: "users_type",
+                keyColumn: "name",
+                keyValue: "BASE");
+
+            migrationBuilder.DeleteData(
+                table: "users_type",
+                keyColumn: "name",
+                keyValue: "PREMIUM");
+
+            migrationBuilder.DeleteData(
+                table: "prices_type",
+                keyColumn: "name",
+                keyValue: "CHARGING");
+
+            migrationBuilder.DeleteData(
+                table: "prices_type",
+                keyColumn: "name",
+                keyValue: "PARKING");
+
+            migrationBuilder.DeleteData(
+                table: "slots_status",
+                keyColumn: "status",
+                keyValue: "FREE");
+
+            migrationBuilder.DeleteData(
+                table: "users_type",
+                keyColumn: "name",
+                keyValue: "ADMIN");
+        }
+    }
+}
